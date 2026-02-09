f2c4dd0e14248e59acdfdac5f45b671e9f4d33cb
imilos <imilos@gmail.com>
2026-02-09 22:15:41 +0100

    Environment returned

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://researchers-api.unic.kg.ac.rs/api',
+  //apiUrl: 'http://147.91.204.111:8000/api',
+  orcidurl: 'https://orcid.org/',
+  scopusurl: 'https://www.scopus.com/authid/detail.uri?authorId=',
+  ecrisurl: 'https://cris.cobiss.net/e-cris/sr/sr_latn/researcher/code/',
+  unikgurl: 'https://www.kg.ac.rs/nastavnici_nastavnik.php?ib_je=',
+  scidarurl: 'https://scidar.kg.ac.rs/browse?type=author&authority='
+};
